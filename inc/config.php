c87b3639dc8afd11e5490f8738c6d23441993782
Trey Brister <webstandardcss@gmail.com>
2017-08-28 00:49:31 -0500

    create config file with site data

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php
+$siteName = "McDaniel's Auto Painting and Body Work";
+$siteTwitter = "@mcdanielsqbw";
+$siteKeywords = ",auto,body shop,auto painting";
+$siteFacebookAdmin = "Facebook admin numeric ID";
+$siteHost = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI];
+$pageURL = $siteHost . $_SERVER[REQUEST_URI]";
+$pageImage = $siteHost . "images/logo.png";
+$pageDateModifiedFriendly = date("F d Y H:i:s",filemtime($pageURL));
+// February 14 2006 13:22:46
+$pageDateModified = date("Y-F-d\TH:i:s-5:00",filemtime($pageURL));
+// 2013-09-17T05:59:00+01:00
+?>
