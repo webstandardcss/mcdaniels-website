4972c7a308cbf5ffbb147b74651e179962919e49
Trey Brister <webstandardcss@gmail.com>
2017-09-02 15:10:57 -0500

    Add buttons to homepage

--- index.php
+++ index.php
@@ -7,10 +7,18 @@ $pageKeywords = "homepage" . $siteKeywords;
 ?>
 
 <?php include_once("inc/header.php"); ?>
-
+<?php // .columns.is-multiline.home-buttons.is-mobile.is-hidden-desktop.is-gapless.has-text-centered.is-size-5.is-size-6-mobile>.column.is-one-third.home-buttons-column.home-buttons-column-$*6>img[src="images/icon-home-estimates.png" alt="Estimates"]+span.icon-home-label.icon-home-label-${Estimates} ?>
 <div class="column mc-content-body section">
-  <h1 class="title">Welcome to McDaniel&rsquo;s Quality Body Works</h1>
+  <div class="columns is-multiline home-buttons is-mobile is-hidden-desktop is-gapless has-text-centered is-size-5 is-size-6-mobile">
+    <div class="column is-one-third home-buttons-column home-buttons-column-1"><img src="images/icon-home-estimates.png" alt="Estimates"><span class="icon-home-label icon-home-label-1">Estimates</span></div>
+    <div class="column is-one-third home-buttons-column home-buttons-column-2"><img src="images/icon-home-faq.png" alt="Accident FAQ&rsquo;s"><span class="icon-home-label icon-home-label-2">Accident FAQ&rsquo;s</span></div>
+    <div class="column is-one-third home-buttons-column home-buttons-column-3"><img src="images/icon-home-warranty.png" alt="Warranty"><span class="icon-home-label icon-home-label-3">Warranty</span></div>
+    <div class="column is-one-third home-buttons-column home-buttons-column-4"><img src="images/icon-home-location.png" alt="Directions"><span class="icon-home-label icon-home-label-4">Directions</span></div>
+    <div class="column is-one-third home-buttons-column home-buttons-column-5"><img src="images/icon-home-icar.png" alt="I-Car Gold Training"><span class="icon-home-label icon-home-label-5">I-Car Gold Training</span></div>
+    <div class="column is-one-third home-buttons-column home-buttons-column-6"><img src="images/icon-home-history.png" alt="History"><span class="icon-home-label icon-home-label-6">History</span></div>
+  </div>
   <div>
+    <h1 class="title">Welcome to McDaniel&rsquo;s Quality Body Works</h1>
     <h2 class="subtitle"><img src="images\vlmcdaniel.png" class="is-pulled-right" alt="V.L. McDaniel">McDaniel's Quality Body Works is one of the oldest paint and body shops in this part of the country, established in 1933 and continued for three generations.</h2>
     <p class="block">Original founder V.L. McDaniel started in the collision repair business in Abilene, Texas in 1921 as a Body Technician and Shop Manager. After relocating to the East Texas area in 1930, V.L. went to work at a Chevrolet dealership in Overton as a Body Technician and Shop Manager and later came to Le Sage Chevrolet in Longview in 1932&hellip; <a href="#">Read More</a></p>
     <h3 class="block subtitle">Accident? Insurance Claim?</h3>
