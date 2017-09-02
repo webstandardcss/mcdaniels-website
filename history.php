840a703424c71fb8071114355e5afb5cb1157180
Trey Brister <webstandardcss@gmail.com>
2017-09-02 02:39:12 -0500

    create history page

---
+++ history.php
@@ -0,0 +1,28 @@
+
+<?php // Page data
+include_once("inc/config.php");
+$pageTitle = $siteName . " Homepage";
+$pageDescription = $siteName . "Homepage";
+$pageKeywords = "homepage" . $siteKeywords;
+?>
+
+<?php include_once("inc/header.php"); ?>
+
+<div class="column mc-content-body section">
+  <div class="columns">
+    <div class="column"><img src="images/history-1-vl.jpg" alt="VL McDaniels"></div>
+    <div class="column"><img src="images/history-2-richard.jpg" alt="Richard McDaniels"></div>
+    <div class="column"><img src="images/history-3-dickie.jpg" alt="Dickie McDaniels"></div>
+    <div class="column"><img src="images/history-4-darrell.jpg" alt="Darrell Smith"></div>
+  </div>
+  <div>
+    <h1 class="title">History of McDaniel&rsquo;s Quality Body Works</h1>
+    <h2 class="subtitle">McDaniel's Quality Body Works is one of the oldest paint and body shops in this part of the country, established in 1933 and continued for three generations.</h2>
+    <p class="block">Original founder V.L. McDaniel started in the collision repair business in Abilene, Texas in 1921 as a Body Technician and Shop Manager. After relocating to the East Texas area in 1930, V.L. went to work at a Chevrolet dealership in Overton as a Body Technician and Shop Manager and later came to Le Sage Chevrolet in Longview in 1932 serving as Shop Manager. As son Richard McDaniels was recalling these days in the early 1930's during his childhood, several fond memories came to mind, especially the story of his dad coming home to boast that he was repairing a vehicle for the infamous pair, Bonnie and Clyde.</p>
+    <p class="block">Not only did Richard say the times were different then, but the cars have come a long way also. He recalls the days when the upper part of the cars were made of wood and cloth and technicians were referred to as "Top and Bodymen" and "they had to be as good with a sewing machine as any other tool they had." Not only were the cars different, but the paint as well. Richard recalls that vehicles were painted by hand through the 1920's and that it was only in the early 1930's that his father was finally trained by General Motors to spray paint cars. After gaining the experience, V.L. finally ventured on his own in 1933, starting the first McDaniel's Body Shop on Tyler Steet near downtown Longview. Because he was too old for the draft in World War II, V.L. continued his repair shop and was the only reputable shop left in Longview during the war, so business continued to grow.</p>
+    <p class="block">In 1945, while away during the war on a merchant ship, Richard received a letter from his father telling him he wanted to purchase property on Hiway 80 and build a new shop. The deal was reached and the building was built in 1946, opened in early 1947 and continues to be our office today. In 1947, Richard returned from the war to help his father at the new location of Quality Body Works where 24 hour towing, service station, used car sales as well as paint and body repairs were offered.</p>
+    <p class="block">After the passing of V.L. in 1972, Richard continued to own and operate the shop until 1973 when the reins were passed to sons Richard Jr.(Dickie) and Mark McDaniel and was later renamed McDaniel's Quality Body Works in 1975. Dickie worked along side of Mark until 1987, when he ventured on his own into paint and bodyshop equipment sales. Richard retired in 1992, but still continues to come by weekly to "check on things." Mark McDaniel continued to operate the shop until September 2011 when he retired and the reigns of ownership were passed to longtime Sales Manager, Darrell Smith.</p>
+  </div>
+</div>
+
+<?php include_once("inc/footer.php"); ?>
