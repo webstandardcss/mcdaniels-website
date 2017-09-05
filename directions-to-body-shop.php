869d57acae6ed4d44a83655a343629528159d11c
Trey Brister <webstandardcss@gmail.com>
2017-09-05 01:16:47 -0500

    create pages for the two locations - Longview and Kilgore

---
+++ directions-to-body-shop.php
@@ -0,0 +1,49 @@
+
+<?php // Page data
+include_once("inc/config.php");
+$pageTitle = "Directions to our Longview Location";
+$pageTitleTag = $siteName . ": " . $pageTitle;
+$pageDescription = $siteName  . ": " . $pageTitle;
+$pageKeywords = $pageTitle . ",directions, Longview Texas" . $siteKeywords;
+?>
+
+<?php include_once("inc/header.php"); ?>
+<?php // .columns.is-multiline.home-buttons.is-mobile.is-hidden-desktop.is-gapless.has-text-centered.is-size-5.is-size-6-mobile>.column.is-one-third.home-buttons-column.home-buttons-column-$*6>img[src="images/icon-home-estimates.png" alt="Estimates"]+span.icon-home-label.icon-home-label-${Estimates} ?>
+<div class="column mc-content-body section">
+  <h1 class="title"><?php echo $pageTitle; ?></h1>
+  <h2 class="subtitle">Auto Body Repair Warranty in Longview Texas</h2>
+  <div class="columns">
+    <div class="column is-narrow">
+      <a href="mcdaniels-kilgore-location.php" target="_blank" class="image sidebar-image">
+        <img src="images\sidebar-locations.png" alt="<?php echo $pageTitle; ?>">
+      </a>
+    </div>
+    <div class="column">
+      <p class="block is-hidden-mobile" style="padding-bottom:1%"></p>
+      <div class="vcard">
+       <h1 class="note is-hidden-fullhd is-hidden-mobile">McDaniels Quality Body Works - Longview Auto Body Repair - Longview Auto Painting - Longview Collision Repair - Longview Body Shop</h1>
+       <h4 class="org fn"><a href="index.php" class="url">McDaniels Quality Body Works</a></h4>
+       <div class="adr"> <span class="street-address">404 E. Marshall</span> <span class="locality">Longview</span>, <span class="region">Texas</span>, <span class="postal-code">75601</span>
+         <!-- .adr -->
+       </div>
+       <div>
+         <div title="+1.903.758.5033" class="tel first"><span class="type null">Work</span>: 903-758-5033</div>
+         <div title="+1.903.753.5391" class="tel"><span class="type null">Pref</span>: 903-753-5391</div>
+         <div class="tel" title="+1.903.758.8290"><span class="type">Fax</span>: 903-758-8290</div>
+         <div>Mon &ndash; Fri 7:30 &ndash; 5:30 </div>
+         <a href="mailto:darrell-mcdaniels@sbcglobal.net" class="email">
+         <!--Email Me-->
+         </a>
+         <!-- &mdash; -->
+         <!--<a href="http://feeds.technorati.com/contacts/http://www.easttexasautos.com/autobody/index.php" class="add-contact">Add to Address Book</a>-->
+          <!-- .vcard -->
+         </div>
+       </div>
+
+      <p class="block"></p>
+    </div>
+  </div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d1001.5503395038198!2d-94.85502098035244!3d32.39771289066999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d32.397989599999995!2d-94.8551411!5e0!3m2!1sen!2sus!4v1497983676654" width="400" height="300" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+</div>
+
+<?php include_once("inc/footer.php"); ?>
---
+++ mcdaniels-kilgore-location.php
@@ -0,0 +1,46 @@
+
+<?php // Page data
+include_once("inc/config.php");
+$pageTitle = "Directions to our Kilgore Location";
+$pageTitleTag = $siteName . " " . $pageTitle;
+$pageDescription = $siteName  . " " . $pageTitle;
+$pageKeywords = $pageTitle . ",limited warranty, limited lifetime warranty" . $siteKeywords;
+?>
+
+<?php include_once("inc/header.php"); ?>
+<?php // .columns.is-multiline.home-buttons.is-mobile.is-hidden-desktop.is-gapless.has-text-centered.is-size-5.is-size-6-mobile>.column.is-one-third.home-buttons-column.home-buttons-column-$*6>img[src="images/icon-home-estimates.png" alt="Estimates"]+span.icon-home-label.icon-home-label-${Estimates} ?>
+<div class="column mc-content-body section">
+  <h1 class="title"><?php echo $pageTitle; ?></h1>
+  <h2 class="subtitle">McDaniel's Quality Body Work's new location is now open at All-Star Ford in Kilgore TX.</h2>
+  <div class="columns">
+    <div class="column is-narrow">
+      <a href="directions-to-body-shop.php" target="_blank" class="image sidebar-image">
+        <img src="images\sidebar-locations.png" alt="<?php echo $pageTitle; ?>">
+      </a>
+    </div>
+    <div class="column">
+      <p class="block is-hidden-mobile" style="padding-bottom:1%"></p>
+      <div class="vcard">
+        <h1 class="note is-hidden-fullhd is-hidden-mobile">McDaniels Quality Body Works - Longview Auto Body Repair - Longview Auto Painting - Longview Collision Repair - Longview Body Shop</h1>
+        <h4 class="org fn"><a href="index.php" class="url">McDaniels Quality Body Works</a></h4>
+        <div class="adr"> <span class="street-address">1615 Hwy 259 N</span> <span class="locality">Kilgore</span>, <span class="region">Texas</span>, <span class="postal-code">75662</span>
+          <!-- .adr -->
+        </div>
+        <div><abbr title="+1.903.218.6099" class="tel first"><span class="type null">Work</span>: 903-218-6099</abbr></div>
+        <div class="tel" title="+1.903.758.8290"><span class="type">Fax</span>: 903-758-8290</div>
+        <div>Mon &ndash; Fri 7:30 &ndash; 5:30 </div>
+        <a href="mailto:darrell-mcdaniels@sbcglobal.net" class="email">
+        <!--Email Me-->
+        </a>
+        <!-- &mdash; -->
+        <!--<a href="http://feeds.technorati.com/contacts/http://www.easttexasautos.com/autobody/index.php" class="add-contact">Add to Address Book</a>-->
+        <!-- .vcard -->
+      </div>
+
+      <p class="block"></p>
+    </div>
+  </div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d1001.5503395038198!2d-94.85502098035244!3d32.39771289066999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d32.397989599999995!2d-94.8551411!5e0!3m2!1sen!2sus!4v1497983676654" width="400" height="300" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+</div>
+
+<?php include_once("inc/footer.php"); ?>
