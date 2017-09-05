69e3761cb2666ce9c8eaca8faadb738c4bcdaa89
Trey Brister <webstandardcss@gmail.com>
2017-09-04 21:06:12 -0500

    create sikkens page

---
+++ sikkens-refinish.php
@@ -0,0 +1,29 @@
+
+<?php // Page data
+include_once("inc/config.php");
+$pageTitle = "Sikkens Refinish Products";
+$pageTitleTag = $pageTitle . " proudly used at " . $siteName;
+$pageDescription = $pageTitle . " proudly used at " . $siteName;
+$pageKeywords = $pageTitle . "" . $siteKeywords;
+?>
+
+<?php include_once("inc/header.php"); ?>
+<?php // .columns.is-multiline.home-buttons.is-mobile.is-hidden-desktop.is-gapless.has-text-centered.is-size-5.is-size-6-mobile>.column.is-one-third.home-buttons-column.home-buttons-column-$*6>img[src="images/icon-home-estimates.png" alt="Estimates"]+span.icon-home-label.icon-home-label-${Estimates} ?>
+<div class="column mc-content-body section">
+  <h1 class="title"><?php echo $pageTitle; ?></h1>
+  <h2 class="subtitle"><?php echo $pageTitleTag; ?> in Longview and Kilgore Texas</h2>
+  <div class="columns">
+    <div class="column is-narrow">
+      <img src="images\sidebar-sikkens.png" class="sidebar-image" alt="<?php echo $pageTitleTag; ?>">
+    </div>
+    <div class="column">
+      <p class="block">We have proudly sprayed Sikkens paint for over 20 years, Sikkens is viewed by most to be the best in the collision repair industry.</p>
+      <p class="block">Sikkens paint is owned by Akzo Nobel which has been in the the coatings business for over 200 years and the automotive paint industry for over 100 years.</p>
+      <p class="block">Due to our volume, we are able to keep a well stocked cabinet of tint colors and are able to hand mix your color as needed. Sikkens offers a paint matching formula to us for all makes and models and we are able to create your color in only the amount we need, in house. The capability to mix our own paint gives us the ability to make test panels with your color and tint in house to match as necessary.</p>
+      <p class="block">Sikkens offers one of the best <a href="body-repair-warranty.php">paint warranties</a> in the business. Any vehicle painted by McDaniel's is backed with a <a href="body-repair-warranty.php">paint workmanship warranty</a> for as long as you own the vehicle.</p>
+      <p></p>
+    </div>
+  </div>
+</div>
+
+<?php include_once("inc/footer.php"); ?>
