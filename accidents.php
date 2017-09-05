6bd48977e740f26a0910832108a3eaffa963ce8a
Trey Brister <webstandardcss@gmail.com>
2017-09-04 21:04:29 -0500

    Create accidents page

---
+++ accidents.php
@@ -0,0 +1,35 @@
+
+<?php // Page data
+include_once("inc/config.php");
+$pageTitle = "Accident Help";
+$pageTitleTag = $siteName . " from " . $pageTitle;
+$pageDescription = $siteName . " from " . $pageTitle;
+$pageKeywords = $pageTitle . ",accident,accidents,collision,collisions" . $siteKeywords;
+?>
+
+<?php include_once("inc/header.php"); ?>
+<?php // .columns.is-multiline.home-buttons.is-mobile.is-hidden-desktop.is-gapless.has-text-centered.is-size-5.is-size-6-mobile>.column.is-one-third.home-buttons-column.home-buttons-column-$*6>img[src="images/icon-home-estimates.png" alt="Estimates"]+span.icon-home-label.icon-home-label-${Estimates} ?>
+<div class="column mc-content-body section">
+  <h1 class="title"><?php echo $pageTitle; ?></h1>
+  <h2 class="subtitle">Being involved in an automobile accident can be a very stressful and trying time.</h2>
+  <div class="columns">
+    <div class="column is-narrow">
+      <img src="images\sidebar-help.png" class="sidebar-image" alt="<?php echo $pageTitle; ?>">
+    </div>
+    <div class="column">
+      <p class="block is-hidden-mobile" style="padding-bottom:0"></p>
+      <p class="block">However, with the help of your insurance agent, claim staff and experienced collision center, your worries can be minimized.</p>
+      <p class="block">If you are involved in an accident, always contact the local police department from the scene so that a report can be made. The police department will take care of documenting the parties and vehicles involved along with an account of how the accident happened. While you are waiting on the officer to respond, make sure you find out if there were any witnesses to the accident and ask them to stay or leave a phone number so the officer may get their input.</p>
+      <p class="block">Always take a few minutes and walk around your vehicle and the other vehicle involved making mental notes of any and all damage that may have occurred and consider writing the information down for further reference.</p>
+      <p class="block">If your vehicle is not drivable, the officer will help you make the necessary arrangements to have your vehicle towed. Make sure you get the information from the tow driver as to where your vehicle will be stored. Before you leave the scene, the officer should give you the information of how to obtain the report and when it will be ready.</p>
+      <p class="block">If your accident has occurred on private property, you will find the police department may not respond. In this case, you will need to exchange information with the other party and try to reach an agreement on who was at fault. If you feel the other party is at fault but they are not cooperative, contact your own insurance company and give them all of the information and let their claim department try to resolve the issue for you.</p>
+      <p class="block">After your accident, contact the responsible insurance company as soon as possible, whether it is your insurance or the other party’s involved. It is not always necessary to contact your insurance company if you were not at fault but you may want to call your agent to find out what your policy dictates. Once your claim is filed, give the adjuster all of the information you have, in great detail, including how and when the accident report will be available.</p>
+      <p class="block">Once your claim has been filed and the insurance company gives you the approval to have your vehicle repaired, they will direct you on their claim process and how to get your vehicle repaired. Very few companies today will require you to go get three estimates. Most companies have direct repair programs with body shops, have their own staff adjusters or work with local independent adjusters to get your vehicle repaired.</p>
+      <p class="block">Please see our <a href="/faq.php">FAQ's</a> below for additional help or call the office at <a href="tel:+19037535391">(903) 753-5391</a></p>
+      <h2 class="subtitle"><a href="/faq.php">Frequently Asked Questions</a></h2>
+      <p></p>
+    </div>
+  </div>
+</div>
+
+<?php include_once("inc/footer.php"); ?>
