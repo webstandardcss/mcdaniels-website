b71d147709285b377572bcd3907cd35610246ad6
Trey Brister <webstandardcss@gmail.com>
2017-09-04 23:25:21 -0500

    include fotorama in the header, fix mobile responsiveness of the top row of pictures and add gallery underneath

--- mcdaniels-history.php
+++ mcdaniels-history.php
@@ -8,9 +8,9 @@ $pageKeywords = $pageTitle . ",history" . $siteKeywords;
 ?>
 
 <?php include_once("inc/header.php"); ?>
-
+<?php include_once('inc/fotorama.php') ?>
 <div class="column mc-content-body section">
-  <div class="columns">
+  <div class="columns is-flex-mobile">
     <div class="column"><img src="images/history-1-vl.jpg" alt="VL McDaniels"></div>
     <div class="column"><img src="images/history-2-richard.jpg" alt="Richard McDaniels"></div>
     <div class="column"><img src="images/history-3-dickie.jpg" alt="Dickie McDaniels"></div>
@@ -23,6 +23,17 @@ $pageKeywords = $pageTitle . ",history" . $siteKeywords;
     <p class="block">Not only did Richard say the times were different then, but the cars have come a long way also. He recalls the days when the upper part of the cars were made of wood and cloth and technicians were referred to as "Top and Bodymen" and "they had to be as good with a sewing machine as any other tool they had." Not only were the cars different, but the paint as well. Richard recalls that vehicles were painted by hand through the 1920's and that it was only in the early 1930's that his father was finally trained by General Motors to spray paint cars. After gaining the experience, V.L. finally ventured on his own in 1933, starting the first McDaniel's Body Shop on Tyler Steet near downtown Longview. Because he was too old for the draft in World War II, V.L. continued his repair shop and was the only reputable shop left in Longview during the war, so business continued to grow.</p>
     <p class="block">In 1945, while away during the war on a merchant ship, Richard received a letter from his father telling him he wanted to purchase property on Hiway 80 and build a new shop. The deal was reached and the building was built in 1946, opened in early 1947 and continues to be our office today. In 1947, Richard returned from the war to help his father at the new location of Quality Body Works where 24 hour towing, service station, used car sales as well as paint and body repairs were offered.</p>
     <p class="block">After the passing of V.L. in 1972, Richard continued to own and operate the shop until 1973 when the reins were passed to sons Richard Jr.(Dickie) and Mark McDaniel and was later renamed McDaniel's Quality Body Works in 1975. Dickie worked along side of Mark until 1987, when he ventured on his own into paint and bodyshop equipment sales. Richard retired in 1992, but still continues to come by weekly to "check on things." Mark McDaniel continued to operate the shop until September 2011 when he retired and the reigns of ownership were passed to longtime Sales Manager, Darrell Smith.</p>
+  </div>
+
+  <!-- 2. Add images to <div class="fotorama"></div>. -->
+  <div class="fotorama" data-nav="thumbs">
+    <img src="images/auto-body/history/old-001.jpg" alt="Auto body history">
+    <img src="images/auto-body/history/old-002.jpg" alt="Auto repair history">
+    <img src="images/auto-body/history/old-003.jpg" alt="Old auto body shop">
+    <img src="images/auto-body/history/old-004.jpg" alt="Historical auto restoration">
+    <img src="images/auto-body/history/old-005.jpg" alt="Old Longview Texas">
+    <img src="images/auto-body/history/old-006.jpg" alt="Longview Texas historical photo">
+    <img src="images/auto-body/history/old-007.jpg" alt="Pictures of old Longview Texas">
   </div>
 </div>
 
