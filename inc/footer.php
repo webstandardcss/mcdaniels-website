10d7fdbdb2faf7ebb170b4d0eb85786b3ec865a5
Trey Brister <webstandardcss@gmail.com>
2017-09-05 00:00:39 -0500

    fix footer links and links to html and css validator

--- inc/footer.php
+++ inc/footer.php
@@ -11,7 +11,14 @@
       Hwy 259 N, Kilgore, Texas 75662 &mdash; 903&nbsp;758&nbsp;5033
     </div>
     <div class="block">
-      About - Estimates - I-car Gold Training - Accident & FAQs - Warranty -
+      <!-- About - Estimates - I-car Gold Training - Accident & FAQs - Warranty -->
+      <a href="/about.php">About</a>&nbsp;&nbsp;&mdash;&nbsp;
+      <a href="/auto-body-estimates.php">Estimates</a>&nbsp;&nbsp;&mdash;&nbsp;
+      <a href="/i-car-gold-training.php">I-car Gold Training</a>&nbsp;&nbsp;&mdash;&nbsp;
+      <a href="/accidents.php">Help &amp; FAQs</a>&nbsp;&nbsp;&mdash;&nbsp;
+      <a href="/body-repair-warranty.php">Warranty</a>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;
+      <a href="http://validator.w3.org/check?uri=referer" class="html is-hidden-tablet" title="html">xhtml</a>&nbsp;
+      <a href="http://jigsaw.w3.org/css-validator/check/referer" class="css is-hidden-tablet" title="css">css</a>&nbsp;
       <a class="icon" href="https://facebook.com/">
         <i class="fa fa-facebook"></i>
       </a>
@@ -25,6 +32,11 @@
 </footer>
 
 <?php include_once("script.php"); ?>
+
+
+
+
+
 
 </body>
 </html>
