b6f568063dfb71ed42f21895c03ac2ae700d6744
Trey Brister <webstandardcss@gmail.com>
2017-09-04 23:22:01 -0500

    change config to use thumbnail navigation instead of dots

--- inc/fotorama.php
+++ inc/fotorama.php
@@ -1,10 +1,11 @@
 <!-- 1. Link to jQuery (1.8 or later), -->
+<!-- Usage:  <?php // include_once('inc/fotorama.php'); ?> -->
 <!-- fotorama.css & fotorama.js. -->
 <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
 <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script> <!-- 16 KB -->
 <?php /*
 <!-- 2. Add images to <div class="fotorama"></div>. -->
-<div class="fotorama">
+<div class="fotorama" data-nav="thumbs">
   <img src="http://s.fotorama.io/1.jpg">
   <img src="http://s.fotorama.io/2.jpg">
 </div>
