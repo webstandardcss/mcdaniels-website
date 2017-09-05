45d78b783bdddbac3540e3501ed42ed47a85fd89
Trey Brister <webstandardcss@gmail.com>
2017-09-04 22:52:10 -0500

    fotorama?

---
+++ inc/fotorama.php
@@ -0,0 +1,11 @@
+<!-- 1. Link to jQuery (1.8 or later), -->
+<!-- fotorama.css & fotorama.js. -->
+<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script> <!-- 16 KB -->
+<?php /*
+<!-- 2. Add images to <div class="fotorama"></div>. -->
+<div class="fotorama">
+  <img src="http://s.fotorama.io/1.jpg">
+  <img src="http://s.fotorama.io/2.jpg">
+</div>
+*/
