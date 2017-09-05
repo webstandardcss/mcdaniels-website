3d9358e43414ee1fddb864f0856e409f5d6e7b64
Trey Brister <webstandardcss@gmail.com>
2017-09-05 01:26:36 -0500

    make the phone numbers clickable in the directions pages

--- mcdaniels-kilgore-location.php
+++ mcdaniels-kilgore-location.php
@@ -26,7 +26,7 @@ $pageKeywords = $pageTitle . ",limited warranty, limited lifetime warranty" . $s
         <div class="adr"> <span class="street-address">1615 Hwy 259 N</span> <span class="locality">Kilgore</span>, <span class="region">Texas</span>, <span class="postal-code">75662</span>
           <!-- .adr -->
         </div>
-        <div><abbr title="+1.903.218.6099" class="tel first"><span class="type null">Work</span>: 903-218-6099</abbr></div>
+        <div><abbr title="+1.903.218.6099" class="tel first"><span class="type null">Work</span>: <a href="tel:+19032186099">903-218-6099</a></abbr></div>
         <div class="tel" title="+1.903.758.8290"><span class="type">Fax</span>: 903-758-8290</div>
         <div>Mon &ndash; Fri 7:30 &ndash; 5:30 </div>
         <a href="mailto:darrell-mcdaniels@sbcglobal.net" class="email">
