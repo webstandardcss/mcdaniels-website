beb93043b2ce372943c5b8c6b922907aaff51553
Trey Brister <webstandardcss@gmail.com>
2017-12-09 19:04:34 -0600

    fix google map

--- mcdaniels-kilgore-location.php
+++ mcdaniels-kilgore-location.php
@@ -39,7 +39,7 @@ $pageKeywords = $pageTitle . ",limited warranty, limited lifetime warranty" . $s
       <p class="block"></p>
     </div>
   </div>
-<iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d1001.5539195038198!2d-94.85502098035244!3d32.39771289066999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d32.397989599999995!2d-94.8551411!5e0!3m2!1sen!2sus!4v1497983676654" width="400" height="300" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d1001.5503395038198!2d-94.85502098035244!3d32.39771289066999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d32.397989599999995!2d-94.8551411!5e0!3m2!1sen!2sus!4v1497983676654" width="400" height="300" frameborder="0" style="border:0" allowfullscreen=""></iframe>
 </div>
 
 <?php include_once("inc/footer.php"); ?>
